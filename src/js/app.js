a37d208f16d14a15a40baaa9f14e72a8fa27043e
Igor Konovodov <konovodov@list.ru>
2024-12-05 23:52:21 +0300

    fix: modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,28 @@
 const burger = document.getElementById("burger-toggle");
 const img = document.querySelector(".hero__container");
 const main = document.getElementById("main");
-console.log(main.children);
+const dialog = document.querySelector(".modal");
+const openModal = document.querySelector(".hero__button");
+const closeModal = document.querySelector(".modal__close-button");
 
-const fu = (e) => {
+const openModalAndBlockScroll = () => {
+  dialog.showModal();
+  document.body.classList.add("scroll-block");
+  document.body.classList.add("blur");
+};
+
+const returnScroll = () => {
+  document.body.classList.remove("scroll-block");
+  document.body.classList.remove("blur");
+};
+
+const close = () => {
+  dialog.close();
+  returnScroll();
+  document.body.classList.remove("blur");
+};
+
+const addOrRemoveBlur = (e) => {
   if (e.target.checked) {
     for (let el of main.children) {
       el.classList.add("blur");
@@ -14,4 +33,17 @@ const fu = (e) => {
     }
   }
 };
-burger.addEventListener("change", fu);
+
+const closeByOverlayClick = ({ currentTarget, target }) => {
+  const dialog = currentTarget;
+  const isOverlayClick = target === dialog;
+  if (isOverlayClick) {
+    close();
+  }
+};
+
+burger.addEventListener("change", addOrRemoveBlur);
+openModal.addEventListener("click", openModalAndBlockScroll);
+closeModal.addEventListener("click", close);
+dialog.addEventListener("click", closeByOverlayClick);
+dialog.addEventListener("cancel", returnScroll);
